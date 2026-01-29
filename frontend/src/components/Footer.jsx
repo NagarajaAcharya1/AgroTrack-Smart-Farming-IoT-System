c42bafea27791b7fc8a82fea5bba33b69a2acb59
NagarajaAcharya1 <nagarajaacharya517@gmail.com>
2026-01-30 01:46:28 +0530

    Updated Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -64,11 +64,11 @@ const Footer = () => {
                         <ul className="space-y-4">
                             <li className="flex items-start gap-3">
                                 <MapPin size={20} className="text-agro-500 shrink-0 mt-1" />
-                                <span>123 AgTech Blvd,<br />Innovation Valley, CA 94025</span>
+                                <span>Adyar, Mangalore, Karnataka, India</span>
                             </li>
                             <li className="flex items-center gap-3">
                                 <Phone size={20} className="text-agro-500 shrink-0" />
-                                <span>+1 (555) 123-4567</span>
+                                <span>+91 9876543210</span>
                             </li>
                             <li className="flex items-center gap-3">
                                 <Mail size={20} className="text-agro-500 shrink-0" />
